01f49f7c14002a92b18f9932ed142bccb679d7b1
Fabioclericuzi <fabioclericuzilima@hotmail.com>
2024-07-07 18:34:53 -0300

    Inclusão da explicação das exceções para serem mostradas aos usuários no swagger

--- src/main/java/com/br/hotelCalifornia/api/controller/HotelCaliforniaController.java
+++ src/main/java/com/br/hotelCalifornia/api/controller/HotelCaliforniaController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
-
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,6 +23,10 @@ import com.br.hotelCalifornia.api.dto.HotelCaliforniaDto;
 import com.br.hotelCalifornia.domain.service.Services;
 import com.br.hotelCalifornia.infraestructure.model.HotelCaliforniaModel;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 
 
 
@@ -38,13 +42,24 @@ public class HotelCaliforniaController<T>  {
         this.hotelServices = hotelServices;
     }
     
-    @GetMapping
+    @Operation(summary = "Listar todos os hotéis", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Lista de hotéis recuperada com sucesso"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @GetMapping(value = "/listar")
     public List<?> findTodosHoteis(){
         return hotelServices.findTodos();
     }
     
+    @Operation(summary = "Achar hotéis pelo id", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Hotel encontrado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Hotel não encontrado"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping(value="/{id}")
-    public ResponseEntity<HotelCaliforniaDto> findHotelCalifornia(@PathVariable UUID id) {
+    public ResponseEntity<HotelCaliforniaDto> findHotelCalifornia(@Valid @PathVariable UUID id) {
         try {
             HotelCaliforniaDto dto = hotelServices.find(id);
             return ResponseEntity.ok(dto);
@@ -54,19 +69,36 @@ public class HotelCaliforniaController<T>  {
         
     }
     
+    @Operation(summary = "Salvar hotéis", method = "POST")
+    @ApiResponses(value = { 
+    		@ApiResponse(responseCode = "200", description = "Hotel salvo com sucesso"), 
+    		@ApiResponse(responseCode = "400", description = "Erro ao salvar hotel")
+    })
     @PostMapping(value="/salvar")
-    public HotelCaliforniaDto createHotelCalifornia(@RequestBody HotelCaliforniaDto hotelCalifornia){
+    public HotelCaliforniaDto createHotelCalifornia(@Valid @RequestBody HotelCaliforniaDto hotelCalifornia){
         return  hotelServices.create(hotelCalifornia);
     }
-    
+    @Operation(summary = "Deletar hotéis selecionados", method = "DELETE")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Hotel deletado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Erro ao buscar hotel"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Object> deleteHotelCalifornia(@PathVariable UUID id, HotelCaliforniaModel hotelCalifornia){
+    public ResponseEntity<Object> deleteHotelCalifornia(@Valid @PathVariable UUID id, HotelCaliforniaModel hotelCalifornia){
         return hotelServices.deleteHotelCalifornia(id, hotelCalifornia);
             
     }
      
+    @Operation(summary = "Atualizar informações dos hotéis selecionados", method = "PUT")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Hotel atualizado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Hotel não encontrado"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @PutMapping(value = "/{id}")
-     public ResponseEntity<HotelCaliforniaDto> update(@RequestBody HotelCaliforniaDto hotelCalifornia, @PathVariable UUID id) {
+     public ResponseEntity<HotelCaliforniaDto> update(@Valid @RequestBody HotelCaliforniaDto hotelCalifornia, @PathVariable UUID id) {
           try {
                 HotelCaliforniaDto dto = hotelServices.updateHotelCalifornia(id, hotelCalifornia);
                 return ResponseEntity.ok(dto);
@@ -74,11 +106,23 @@ public class HotelCaliforniaController<T>  {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
            }
         }
+    @Operation(summary = "Procurar hotéis pelo CNPJ", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Hotel encontrado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Hotel não encontrado"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping(value="/getcnpj/{cnpj}")        
     public ResponseEntity<HotelCaliforniaModel> buscarPorCnpj(@PathVariable(value="cnpj") String cnpj){
         return ResponseEntity.status(HttpStatus.OK).body(hotelServices.findCnpj(cnpj));
     }
     
+    @Operation(summary = "Procurar hotéis pelo Nome", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Hotel encontrado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Hotel não encontrado"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping(value="/getnome/{nome}")        
     public ResponseEntity<HotelCaliforniaModel> buscarNome(@PathVariable(value="nome") String nome){
         return ResponseEntity.status(HttpStatus.OK).body(hotelServices.findNome(nome));
--- src/main/java/com/br/hotelCalifornia/api/dto/HotelCaliforniaDto.java
+++ src/main/java/com/br/hotelCalifornia/api/dto/HotelCaliforniaDto.java
@@ -2,6 +2,11 @@ package com.br.hotelCalifornia.api.dto;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,10 +21,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class HotelCaliforniaDto {
+		
+		@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 		private UUID id;
 		
+		@NotNull(message="Nome do hotel é obrigatório")
+		@Size(min = 1, max =100, message="O nome do hotel deve ter entre 1 e 100 caracteres")
 		private String nome;
+		
+		@NotNull(message ="A localização do hotel é obrigatória")
+		@Size(min = 1, max = 100, message = "A localidade precisa ter entre 1 e 100 caracteres")
 		private String localizacao;
+		
+		@NotNull(message = "O CNPJ do hotel é obrigatório")
+		@Size(min = 1, max = 14, message = "O CNPJ precisa ter entre 1 e 14 caracteres")
 		private String cnpj;
 		
 		public String getCnpj() {
